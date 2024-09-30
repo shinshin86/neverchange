22d7c9d81112bcd871896ec0b6127b789ac39157
shinshin86 <beagles1986@gmail.com>
2024-10-01 06:25:58 +0900

    not support Safari until a solution is found
    issue: https://github.com/shinshin86/neverchange/issues/6

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,13 +40,6 @@ export default defineConfig({
         headless: true,
       },
     },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-        headless: true,
-      },
-    },
   ],
   webServer: {
     command: 'npm run dev:e2e',
